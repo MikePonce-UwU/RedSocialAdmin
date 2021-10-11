8646f6d3bc9e4c8e45186d47e05f46f21ba276df
MikePonce-UwU <mvallecillo101@gmail.com>
2021-10-11 17:16:17 -0600

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RedSocialAdmin.Startup))]
+namespace RedSocialAdmin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
